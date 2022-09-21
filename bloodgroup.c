a34f3da44765f7b231698d6a5185bc7af8751c42
muhummud <73762302+muhummud@users.noreply.github.com>
2022-09-22 02:54:27 +0700

    Modified by B on bloodgroup.c

--- bloodgroup.c
+++ bloodgroup.c
@@ -5,11 +5,11 @@ void main()
   int i;
   do
   {
-    printf("What is your Crewmate ID? ");
+    printf("What is your Student ID? ");
     scanf("%s", Name);
     do
     {
-      printf("What is your sussy blood group [A, B, AB, or O]? ");
+      printf("What is your parent blood group [A, B, AB, or O]? ");
       scanf("%s", G);
 
       if (strcmp(G, "A") !=0  &&
